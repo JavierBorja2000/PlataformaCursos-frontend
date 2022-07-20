67cd39759b14b59361f2ddc11cb9c8843576868c
lkdubonc <102050221+lkdubonc@users.noreply.github.com>
2022-07-20 15:27:17 -0600

    Agregando vista de factura individual

--- public/js/estudiante/factura.js
+++ public/js/estudiante/factura.js
@@ -1,9 +1,95 @@
 var token;
+var id;
+
+// Datos de factura
+const NumFactura = document.querySelector("#NumFactura");
+const Fecha = document.querySelector("#Fecha");
+const Nit = document.querySelector("#Nit");
+const Nombres = document.querySelector("#Nombres");
+const Apellidos = document.querySelector("#Apellidos");
+const Total = document.querySelector("#Total");
+const Listado = document.querySelector("#listado");
 
 document.addEventListener('DOMContentLoaded', e => {
     ValidarToken("Estudiante");
-    var url = new URL (window.location.href.toLowerCase());
-    var id = url.searchParams.get("id");
+    var url = new URL(window.location.href.toLowerCase());
+    id = url.searchParams.get("id");
 
     console.log(id)
+
+    var factura = {
+        id: 1,
+        fecha: '10/10/2022',
+        nit: 1028,
+        nombres: 'Lesly',
+        apellidos: "Dubón",
+        cursos: [
+            {
+                nombre: "A",
+                descripcion: "A",
+                costo: 30
+            },
+            {
+                nombre: "B",
+                descripcion: "B",
+                costo: 90
+            }
+        ],
+        total: 120
+    }
+
+    // ObtenerFactura();
+    ImprimirFactura(factura);
 })
+
+function ObtenerFactura() {
+    fetch(url_facturas + "/" + id, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    }).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        else {
+            console.log(response)
+        }
+    }).then(function (Data) {
+        ImprimirFactura(Data)
+    });
+}
+
+function ImprimirFactura(factura) {
+    console.log(factura)
+
+    NumFactura.textContent = factura.id;
+    Fecha.textContent = factura.fecha;
+    Nit.textContent = factura.nit;
+    Nombres.textContent = factura.nombres;
+    Apellidos.textContent = factura.apellidos;
+    Total.textContent = factura.total;
+
+    factura.cursos.forEach(curso => {
+        const fila = document.createElement("tr"); 
+        
+        const nombreCurso = document.createElement("td"); 
+        nombreCurso.classList.add("border", "border-violet-800", "px-3", "py-1");
+        nombreCurso.textContent = curso.nombre;
+
+        const descripcion = document.createElement("td"); 
+        descripcion.classList.add("border", "border-violet-800", "px-3", "py-1");
+        descripcion.textContent = curso.descripcion;
+
+        const costo = document.createElement("td"); 
+        costo.classList.add("border", "border-violet-800", "px-3", "py-1");
+        costo.textContent = curso.costo;
+
+        fila.appendChild(nombreCurso);
+        fila.appendChild(descripcion);
+        fila.appendChild(costo);
+        Listado.appendChild(fila);
+    });
+}
--- public/js/estudiante/facturas.js
+++ public/js/estudiante/facturas.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', e => {
         }
     ]
 
+    // ObtenerFacturas();
     ImprimirFacturas(facturas);
 })
 
